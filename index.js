9618566712cd4cc28837408c12c0197fa6a7f148
Ana Amaral <101297541+MachadoA@users.noreply.github.com>
2022-10-06 14:24:02 +0100

    Update index.js
    Alterei o código padrão de forma a conseguir a opção de ter também um evento clique.

--- index.js
+++ index.js
@@ -1,18 +1,41 @@
-function removeTransition(e) {
-    if (e.propertyName !== 'transform') return;
-    e.target.classList.remove('playing');
-  }
+const keys = Array.from(document.querySelectorAll('.key'));
+keys.forEach(key => key.addEventListener('transitionend', removeCSSTransition));
+window.addEventListener('keydown', playSound);
 
-  function playSound(e) {
+//Para ter a opção de responder ao click
+const btns = Array.from(document.getElementsByClassName('key'));
+btns.forEach(btns => btns.addEventListener('click', removeCSSTransition));
+window.addEventListener('click', playSound);
+
+function removeCSSTransition(e) {
+  if ((e.propertyName !== 'transform') && (e.propertyName !== 'click')) return;
+  e.target.classList.remove('playing');
+}
+
+function playSound(e) {
+  //Captura o KeyCode de forma normal por se tratar de evento disparado pelo teclado 'keydown'
+  if (e.type === "keydown"){
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
     if (!audio) return;
+    key.classList.add('playing');
+    audio.currentTime = 0;
+    audio.play();
+  } 
 
+  //Captura o KeyCode de forma diferente por se tratar de evento onClick
+  if (e.type === "click"){
+    const audio = document.querySelector(`audio[data-key="${e.path[1].dataset.key}"]`); 
+    const key = document.querySelector(`div[data-key="${e.path[1].dataset.key}"]`);
+    console.log(e)
+    //objeto PointerEvent
+    //São eventos DOM que são disparados por dispositivos ponteiros (mouse)
+    if (!audio) return;
     key.classList.add('playing');
     audio.currentTime = 0;
     audio.play();
   }
 
-  const keys = Array.from(document.querySelectorAll('.key'));
-  keys.forEach(key => key.addEventListener('transitionend', removeTransition));
-  window.addEventListener('keydown', playSound);
+}
+
+
